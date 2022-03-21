de2e63fc645088313828b41ab9af00c6ceb5a982
Alana Dias <ads.pict@gmail.com>
2022-03-21 14:16:52 -0300

    Criar diretório em novo repositório

--- empresaSpring/empresaFront/src/app/models/cargoModelo.ts
+++
@@ -1,7 +0,0 @@
-export interface Cargo{
-  id_cargo?: any
-  ca_nome: String
-  ca_atribuicao: String
-
-
-}
--- empresaSpring/empresaFront/src/app/models/departamentoModelo.ts
+++
@@ -1,4 +0,0 @@
-export interface Departamento{
-  id_departamento?: any
-  dep_nome: String
-}
